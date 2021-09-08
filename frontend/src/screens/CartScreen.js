a92812cb89f33eee48610621f33a24ac7729e607
silvijazema <silvija.zema@gmail.com>
2021-09-08 22:56:38 +0300

    produktu kiekio pasirinkimas ir krepsio skaiciavimas

---
+++ frontend/src/screens/CartScreen.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function CartScreen(props) {
+    const productId = props.match.params.id;
+    const qty = props.location.search
+    ? Number(props.location.search.split('=')[1])
+    : 1;
+
+    return (
+        <div>
+            <h1>
+                Mano krepšelis
+            </h1>
+            <p>Pridėti į krepšelį: ProductID: {productId} Qty: {qty} </p>
+        </div>
+    );
+}
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,69 +1,116 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { detailsProduct } from '../actions/productActions';
 import { Link } from 'react-router-dom';
+import LoadingBox from '../components/LoadingBox';
+import MessageBox from '../components/MessageBox';
 import Rating from '../components/Rating';
-import data from '../data';
+
 
 export default function ProductScreen(props) {
-  const product = data.products.find((x) => x._id === props.match.params.id);
-  if (!product) {
-    return <div> Product Not Found</div>;
-  }
+  const dispatch = useDispatch();
+  const productId = props.match.params.id;
+  const [qty, setQty] = useState(1);
+  const productDetails = useSelector( state => state.productDetails);
+  const { loading, error, product } = productDetails;
+
+  useEffect(() => {
+    dispatch(detailsProduct(productId));
+  }, [dispatch, productId]); 
+
+const addToCartHandler = () => {
+  props.history.push(`/cart/${productId}?qty=${qty}`);
+};
+
   return (
     <div>
-      <Link to="/">Atgal į visus produktus</Link>
-      <div className="row top boxed-content">
-      
-        <div className="col-1">
-          <img className="large" src={product.image} alt={product.name}></img>
-        </div>
+      {loading ? (
+        <LoadingBox></LoadingBox>
+      ) : error ? (
+        <MessageBox variant="danger">{error}</MessageBox>
+      ) : (
+        <div>
+        <Link to="/">Atgal į visus produktus</Link>
+        <div className="row top boxed-content">
         
-        <div className="col-1">
-          <ul>
-            <li>
-              <h1>{product.name}</h1>
-            </li>
-            <li>
-              <Rating
-                rating={product.rating}
-                numReviews={product.numReviews}
-              ></Rating>
-            </li>
-            <li>Vieneto kaina: ${product.price}</li>
-            <li>
-              Skanėsto aprašymas:
-              <p>{product.description}</p>
-            </li>
-          </ul>
-        </div>
-        <div className="col-1">
-          <div className="card card-body">
+          <div className="col-1">
+            <img className="large" src={product.image} alt={product.name}></img>
+          </div>
+          
+          <div className="col-1">
             <ul>
               <li>
-                <div className="row">
-                  <div>Kaina</div>
-                  <div className="price">${product.price}</div>
-                </div>
+                <h1>{product.name}</h1>
               </li>
               <li>
-                <div className="row">
-                  <div>Ar turime?</div>
-                  <div>
-                    {product.countInStock > 0 ? (
-                      <span className="success">Taip!</span>
-                    ) : (
-                        <span className="danger">Deja ne :(</span>
-                    )}
-                  </div>
-                </div>
-                
+                <Rating
+                  rating={product.rating}
+                  numReviews={product.numReviews}
+                ></Rating>
               </li>
+              <li>Vieneto kaina: ${product.price}</li>
               <li>
-                <button className="primary block">Į krepšelį</button>
+                Skanėsto aprašymas:
+                <p>{product.description}</p>
               </li>
             </ul>
           </div>
+          <div className="col-1">
+            <div className="card card-body">
+              <ul>
+                <li>
+                  <div className="row">
+                    <div>Kaina</div>
+                    <div className="price">${product.price}</div>
+                  </div>
+                </li>
+                <li>
+                  <div className="row">
+                    <div>Ar turime?</div>
+                    <div>
+                      {product.countInStock > 0 ? (
+                        <span className="success">Taip!</span>
+                      ) : (
+                          <span className="danger">Deja ne :(</span>
+                      )}
+                    </div>
+                  </div>
+                  
+                </li>
+                {
+                  product.countInStock > 0 && (  
+                <>
+                <li>
+                  <div className="row">
+                    <div> Qty
+                    </div>
+                    <div>
+                      <select value={qty}
+                      onChange={e => setQty(e.target.value)}>
+                        {
+                          [...Array(product.countInStock).keys()].map( 
+                            (x) => (
+                            <option key={ x + 1 } value={ x + 1}>{x + 1}</option>
+                          ))
+                        }
+                      </select>
+                    </div>
+                  </div>
+                </li>
+                <li>
+                  <button onClick={addToCartHandler} className="primary block">Į krepšelį</button>
+                </li>
+                </>
+               
+                )}
+              </ul>
+            </div>
+          </div>
         </div>
       </div>
+                )}
     </div>
+    
+    
   );
 }
